ed9a9b690f66de93c6a613196628e50ecfe94390
Susanna <susikbabakhanyan@gmail.com>
2020-08-31 16:50:38 +0400

    mnac footery

---
+++ tailwind.config.js
@@ -0,0 +1,104 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        fullColor:{
+          blue: '#639BCD',
+          skyblue: '#D5D7E2',
+          danger: '#A00404'
+        },
+        primary: {
+          dark:'#C5C504',
+          light: '#D6D64F'
+        },
+        secondary:{
+          100:'#40413A',
+          200:'#828282',
+        },
+        bgColor:{
+          100: '#D5D7E2'
+        }
+      },
+      opacity:{
+       '15': '0.15'
+      },
+      fontFamily:{
+        body: ['Merriweather'],
+        sansPro:['"Source Sans Pro"'],
+        crimson: ['"Crimson text"']
+      },
+      fontSize: {
+        '17xl': '1.0625rem',
+        '22xl': '1.375rem',
+        '32xl': '2rem',
+        '36xl': '2.25rem',
+        '52xl': '3.25rem',
+        '54xl': '3.375rem',
+        '6xl': '4rem',
+      },
+      margin:{
+        '60': '3.75rem',
+        '308': '19.25rem',
+        '330': '21.112rem',
+        '241': '241px',
+        '37': '2.3125rem',
+        '54': '3.375rem',
+        '150': '9.375rem',
+        '100': '6.25rem',
+        '30': '1.875rem',
+        'min30': '-1.875rem',
+        'min60': '-3.75rem',
+        '70': '4.375rem'
+      },
+      padding:{
+        '32xl': '1.6875rem',
+        "12xl": '1.125rem',
+        '45xl': '2.8125rem',
+        '56xl' : '3.5rem',
+        '60': '3.75rem',
+        '100': '6.25rem',
+
+      },
+      rounded:{
+        '26': '0.75rem'
+      },
+      height:{
+        '45' : '45px',
+        '60' : '3.75rem',
+        '100': '6.25rem',
+        '270' : '15rem',
+        '415' : '26rem',
+        '150': '9.375rem',
+        '520' : '32.5rem'
+      },
+      width:{
+        '234' : '234px',
+        '376' : '17.5rem',
+        '946': '946px',
+        '816': '816',
+        '690':'43.4375rem',
+        '28' : '1.75rem',
+        '100': '6.25rem',
+        '150': '9.375rem',
+        '615': '35rem',
+        '276': '290px',
+        '340': '340px',
+        '400': '400px',
+        '500': '31.25rem',
+        '667': '41.6875rem',
+        '424' : '26.5rem',
+        '535' : '33.4375rem'
+        // '590': '25rem'
+      },
+      container:{
+        'yes': '1440px'
+      },
+      lineHeight:{
+        '80': '5rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
